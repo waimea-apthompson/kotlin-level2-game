1817357e45e6d77ea4d34c2ee553e6bfcd6f267c
waimea-apthompson <apthompson@waimea.school.nz>
2026-03-23 10:59:43 +1300

    trying to make bored
    trying to make the bored

--- src/Main.kt
+++ src/Main.kt
@@ -2,8 +2,8 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
+ * Project Name:   squeeze
+ * Project Author: austin
  * GitHub Repo:    GITHUB REPO URL HERE
  * ---------------------------------------------------------------------
  * Notes:
@@ -12,8 +12,39 @@
  */
 
 
+val squares = mutableMapOf<String,Int>()
 
 fun main() {
-    println("Hello World!")
+   println("=================")
+    println("=               =")
+    println("= the squeeze   =")
+    println("=               =")
+    println("=================")
+println()
+    println()
+showboreod()
+}
+fun creatbored(){
+    while (squares.size < 6) {
+        squares.add("EMPTY")
+    }
+}
+
+
+fun showboreod(){
+
+
+    println("   cell1    cell2    cell3    cell4    cell5    cell6")
+
+    print("╭--------".repeat(squares.size))
+    println("╮")
+    for (square in squares) {
+
+        print("|  $square".padEnd(9))
+    }
+    println("|")
+    print("╰--------".repeat(squares.size))
+    println("╯")
+
 }
 
