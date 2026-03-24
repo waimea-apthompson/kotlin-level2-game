1b6bd577e1ef1c97e0f0a196106eb1d7c289b67e
waimea-apthompson <apthompson@waimea.school.nz>
2026-03-24 15:08:09 +1300

    sit up chosie, get name and fix my code

--- src/Main.kt
+++ src/Main.kt
@@ -12,8 +12,9 @@
  */
 
 
-val squares = mutableMapOf<String,Int>()
-
+val squares = mutableListOf<String>()
+val p2n = String
+val p1n = String
 fun main() {
    println("=================")
     println("=               =")
@@ -22,11 +23,49 @@ fun main() {
     println("=================")
 println()
     println()
+    creatbored()
+    setupconters()
+    playername()
 showboreod()
+
+
+
+    while (true){
+        val action = player1answer()
+
+        when (action) {
+
+            'L' -> left()
+
+            'R' -> right()
+
+            'S' ->swap()
+        }
+        break
+    }
+
+
+    while (true){
+        val action2 = player2answer()
+
+        when (action2) {
+
+            'L' -> left()
+
+            'R' -> right()
+
+            'S' ->swap()
+        }
+    }
 }
+
+
+
+
 fun creatbored(){
-    while (squares.size < 6) {
-        squares.add("EMPTY")
+    while (squares.size < 15) {
+        squares.add  ("...")
+
     }
 }
 
@@ -34,17 +73,83 @@ fun creatbored(){
 fun showboreod(){
 
 
-    println("   cell1    cell2    cell3    cell4    cell5    cell6")
+    println("   1        2        3        4        5        6       7        8        9        10        11        12       13       14       15")
 
-    print("╭--------".repeat(squares.size))
-    println("╮")
+    print("╔════════".repeat(squares.size))
+    println("╗")
     for (square in squares) {
 
-        print("|  $square".padEnd(9))
+        print("║  $square".padEnd(9))
     }
-    println("|")
-    print("╰--------".repeat(squares.size))
-    println("╯")
+    println("║")
+    print("╚════════".repeat(squares.size))
+    println("╝")
 
 }
 
+
+fun playername(){
+
+    println("player one what  your name?")
+    var p1n = readln()
+
+    println("what a dumb ass name, o and you are white:) aka w")
+    println()
+    println("player two surely you have a better name?")
+    var p2n = readln()
+
+    println("dam it some how worse!, o u black:( aka b")
+}
+
+fun player1answer(): Char{
+    println("what you what to do white?")
+    println("move [L]eft")
+    println("move [R]ight")
+    println("[S]wap")
+    println()
+    val goodchoise = listOf("L, R, S")
+    while (true) {
+        print("chosie: ")
+        val choice = readlnOrNull()?.firstOrNull()
+        if (choice != null) return choice
+    }
+
+}
+
+fun player2answer(): Char{
+    println("what you what to do white?")
+    println("move [L]eft")
+    println("move [R]ight")
+    println("[S]wap")
+    println()
+    val goodchoise = listOf("L, R, S")
+    while (true) {
+        print("chosie: ")
+        val choice = readlnOrNull()?.firstOrNull()
+        if (choice != null) return choice
+    }
+}
+fun setupconters(){
+    val p1Con = "w"
+    val p2Con = "b"
+    squares[6] = p1Con
+    squares[4] = p1Con
+    squares[8] = p1Con
+    squares[9] = p2Con
+    squares[7] = p2Con
+    squares[5] = p2Con
+}
+fun swap(){
+println("under constraction")
+}
+
+fun right(){
+
+
+}
+
+
+fun left(){
+
+
+}
